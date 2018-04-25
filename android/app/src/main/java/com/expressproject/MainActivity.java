59f6a25c5a54189c7537e6d5ccb61bbec816aeea
wanwan710752055@163.com <wanwan710752055@163.com>
2018-04-25 09:43:52 +0800

    react-native-umeng-analytics   友盟统计

--- android/app/src/main/java/com/expressproject/MainActivity.java
+++ android/app/src/main/java/com/expressproject/MainActivity.java
@@ -4,6 +4,19 @@ import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
--- android/app/src/main/java/com/expressproject/MainApplication.java
+++ android/app/src/main/java/com/expressproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.expressproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.example.umenganaticlys.UmengAnalyticsPackage ;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new UmengAnalyticsPackage (),
             new RCTGMBluetoothPackage()
       );
     }
@@ -43,5 +45,7 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    UMConfigure.init(this, key, channelId, UMConfigure.DEVICE_TYPE_PHONE, null);
+    MobclickAgent.openActivityDurationTrack(false);
   }
 }
